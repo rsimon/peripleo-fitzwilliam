c16ce722d3e95ca1616750980cb5f78f82548149
Rainer Simon <aboutgeo@gmail.com>
2022-07-06 11:58:34 +0200

    First experimental crosswalk + demo setup

--- harvest/scripts/src/crosswalk.js
+++ harvest/scripts/src/crosswalk.js
@@ -1,19 +1,29 @@
+import fs from 'fs';
+
+const BASE_PATH = '../data/objects/';
 
 const toArray = x => Array.isArray(x) ? x : [x];
 
-const getFirst = (field, x) => toArray(x)[0][field];
+const getFirst = (field, x) => x ? toArray(x)[0][field] : null;
+
+const files = fs.readdirSync(BASE_PATH);
+
+const features = [];
 
 const crosswalkOne = (idx = 0) => {
+  console.log(`Crosswalking ${idx + 1}/${files.length}`);
 
-  // TODO 
-  const json = {}; // 
+  const str = fs.readFileSync(BASE_PATH + files[idx], 'utf8');
+  const json = JSON.parse(str);
+
   const { data } = json;
 
   const name = getFirst('summary_title', data.name);
 
   const description = getFirst('value', data.description);
   
-  const categories = toArray(data.categories).map(c => c.summary_title);
+  // TODO Fix
+  const categories = toArray(data.categories).map(c => c?.summary_title);
   
   const department = data.department.value;
 
@@ -26,4 +36,40 @@ const crosswalkOne = (idx = 0) => {
 
   // Multimedia
 
+  // Just a hack
+  const collection = data.lifecycle?.collection;
+  if (collection && collection.places?.length > 0) {
+    const { coordinates } = collection.places[0];
+
+    if (coordinates) {
+      features.push({
+        '@id': data.URI,
+        type: 'Feature',
+        title: name,
+        properties: {
+          department
+        },
+        descriptions: [{ value: description }],
+        types: categories.map(value => ({ value })),
+        geometry: {
+          type: 'Point',
+          coordinates: [ parseFloat(coordinates.lng), parseFloat(coordinates.lat) ]
+        }
+      });
+    }
+  }
+
+  if (idx < files.length - 1) {
+    setTimeout(() => crosswalkOne(idx + 1), 1);
+  } else {
+    const geojson = {
+      type: 'FeatureCollection',
+      features
+    };
+
+    fs.writeFileSync('./crosswalked.json', JSON.stringify(geojson, null, 2));
+    console.log('Done.');
+  }
 }
+
+crosswalkOne();
--- public/welcome.md
+++
@@ -1,6 +0,0 @@
-# Welcome to Peripleo
-
-This is an example __welcome message__. Check our 
-[project website](https://britishlibrary.github.io/locating-a-national-collection/maps.html).
-
-Have fun.
